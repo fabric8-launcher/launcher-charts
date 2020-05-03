809400628dacfff276aa3fe1083c38750af57aee
Tako Schotanus <tako@codejive.org>
2020-05-03 02:51:30 +0200

    feat: Added release script

--- update-release-values.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo -e "application:\n  image:\n    tag: $(curl -s https://api.github.com/repos/fabric8-launcher/launcher-application/commits/HEAD | jq -r '.sha' | cut -c1-7)" > release-values.yaml
-echo -e "welcome:\n  image:\n    tag: $(curl -s https://api.github.com/repos/fabric8-launcher/launcher-creator-welcome-app/commits/HEAD | jq -r '.sha' | cut -c1-7)" >> release-values.yaml
